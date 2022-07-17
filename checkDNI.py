89c4663939022177c4767a44c86b591c11da9e93
Alberto González <gonzalezlafuentealberto@gmail.com>
2022-07-17 09:47:49 +0200

    Update checkDNI.py

--- checkDNI.py
+++ checkDNI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from funciones import *
+#from funciones import check_dni_windows
 
 
 
@@ -15,9 +15,18 @@ def check_dni_windows():
     window = Toplevel()
     window.title("Validar DNI")
     window.iconbitmap("files/icono_titulo.ico")
-    window.geometry("640x420")
+    window.geometry("230x160")
     window.resizable(False, False)
+    
+    
+    mensaje1 = Label(window, text="Introduce el DNI: ")
+    mensaje1.grid(row=0, padx=5, pady=5)
 
+    entrada_dni = Entry(window, width=35)
+    entrada_dni.grid(row=1, padx=5, pady=5)
+
+    boton_validar = Button(window, text="VERIFICAR")
+    boton_validar.grid(row=4, padx=20, pady=25)
 
 
 
@@ -30,12 +39,6 @@ boton_check_dni.grid(row=2, column=2, padx=20, pady=25, columnspan=4)
 
 boton_generar_dni = Button(frame_pantalla_principal, text="Generar DNI", command="")
 boton_generar_dni.grid(row=3, column=2, padx=20, pady=5, columnspan=4)
-
-
-
-# Pantalla Validar DNI
-
-
 
 
 
