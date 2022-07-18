e16d881b18197c1f156c15afa084142f8116412a
Alberto González <gonzalezlafuentealberto@gmail.com>
2022-07-18 23:32:02 +0200

    Update checkDNI.py

--- checkDNI.py
+++ checkDNI.py
@@ -3,6 +3,7 @@ from tkinter import *
 from clases import DNI
 from tkinter import messagebox
 #from tkinter import ttk
+from funciones import checker, check_dni_windows
 
 root = Tk()
 root.title("Check DNI")
@@ -11,46 +12,6 @@ root.resizable(False, False)
 root.iconbitmap("files/icono_titulo.ico")
 
 
-
-def checker(num):
-    #messagebox.showinfo(message=num, title="Tests")
-    #print(num)
-    dni = DNI()
-    numero = num
-    num = num[0:8]
-    lett = numero[-1]
-    #print(num)
-    #print(lett)
-    check = dni.check_input_data(num, lett)
-    state = dni.verification(num, lett)
-    print(state)
-    messagebox.showinfo(message=state, title="Estado DNI")
-    #return state
-
-def check_dni_windows():
-    window = Toplevel()
-    window.title("Validar DNI")
-    window.iconbitmap("files/icono_titulo.ico")
-    window.geometry("230x160")
-    window.resizable(False, False)
-    window.grab_set()
-    
-    mensaje1 = Label(window, text="Introduce el DNI: ")
-    mensaje1.grid(row=0, padx=5, pady=5)
-
-    entrada_dni = Entry(window, width=35)
-    entrada_dni.grid(row=1, padx=5, pady=5)
-
-    boton_validar = Button(window, text="VERIFICAR", command= lambda: checker(entrada_dni.get()) )
-    boton_validar.grid(row=4, padx=20, pady=25)
-    
-    salida = Label(window, text ="Esto es una prueba"  )
-    salida.grid(row=5)
-    
-    
-    
-    
-    window.mainloop()
 
 
 # Pantalla Principal
