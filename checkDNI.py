88b605f98d4cb1b302bddbd4fe936df42281b16d
Alberto González <gonzalezlafuentealberto@gmail.com>
2022-07-20 18:19:45 +0200

    Creacion menu basico pantalla principal

--- checkDNI.py
+++ checkDNI.py
@@ -20,6 +20,26 @@ boton_generar_dni = Button(frame_pantalla_principal, text="Generar DNI", command
 boton_generar_dni.grid(row=3, column=2, padx=20, pady=5, columnspan=4)
 
 
+# Menu
+menubar = Menu(root)
+menu_archivo = Menu(menubar, tearoff=0)
+menu_archivo.add_separator()
+menu_archivo.add_command(label="Salir", command=root.quit)
+menubar.add_cascade(label="Archivo", menu=menu_archivo)
+
+menu_ayuda = Menu(menubar, tearoff=0)
+menu_ayuda.add_command(label="Acerca de checkDNI", command="Comando")
+menu_ayuda.add_command(label="Licencia", command="Comando")
+menu_ayuda.add_separator()
+menu_ayuda.add_command(label="Contacto", command="Comando")
+menubar.add_cascade(label="Ayuda", menu=menu_ayuda)
+
+
+
+
+
+root.config(menu=menubar)
+
 
 
 
