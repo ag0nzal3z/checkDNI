714fb6e78b859db592d3c136995c457bc1f2b03f
Alberto González <gonzalezlafuentealberto@gmail.com>
2022-10-12 22:53:57 +0200

    [V0.2 (2022-10-12)]

--- checkDNI.py
+++ checkDNI.py
@@ -58,5 +58,8 @@ def main():
 
 
 if __name__ == "__main__":
-    select_lan = lan.load_languaje('es')
+    #select_lan = lan.load_languaje('es')
+    lan_default = lan.load_default_lenguaje()
+    #select_lan = lan.load_languaje('es')
+    select_lan = lan.load_languaje(lan_default)
     main()
--- files/languajes.py
+++ files/languajes.py
@@ -6,22 +6,30 @@ class Languajes():
     
     def __init__(self):
         self.__lanFile = "files/languajes.json"
-        self.__lanDefault = "es"
+        self.__lanConfDefault = "files/languaje"
     
     
     def load_languaje(self, languaje='es'):
         exist_lan = os.path.isfile(self.__lanFile)
-        #print(exist_lan)
+
         if exist_lan == True:
             idiomas = open(self.__lanFile, 'r')
             diccionario = json.load(idiomas)
             idioma_seleccionado = diccionario[languaje]
+            idiomas.close()
             return idioma_seleccionado
 
 
-
-
-
+    def load_default_lenguaje(self):
+        exist_lan = os.path.isfile(self.__lanConfDefault)
+        if exist_lan == False:
+            file = open(self.__lanConfDefault, 'w')
+            file.write("es")
+        
+        file = open(self.__lanConfDefault, 'r')
+        content = file.readline()
+        file.close()
+        return content
 
 
 # PRUEBAS DURANTE EL DESARROLLO
@@ -31,3 +39,5 @@ if __name__ == "__main__":
     test_lan = lan.load_languaje('es')
     print(test_lan)
     
+    test_lan_defa = lan.load_default_lenguaje()
+    print(test_lan_defa)
